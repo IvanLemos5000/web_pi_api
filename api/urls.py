53811b4835012b0dcfa3ae68960267a3488be2be
Ivan <ivan.grego.lemos@gmail.com>
2019-10-18 20:15:17 -0300

    adjust urls

--- api/urls.py
+++ api/urls.py
@@ -1,51 +1,7 @@
-from django.conf.urls import include, url
-try:
-  from django.conf.urls import patterns
-except ImportError:
-  pass
-import django
-from django.contrib import admin
+from django.conf.urls import url
 from api import views
 
-if django.VERSION[1] < 10:
-  urlpatterns = patterns('',
-  
-    url(r'^addresses/(?P<id>[0-9]+)$', views.AddressesAPIView.as_view()),
-    url(r'^addresses/$', views.AddressesAPIListView.as_view()),
-  
-    url(r'^budgetsituations/(?P<id>[0-9]+)$', views.BudgetSituationsAPIView.as_view()),
-    url(r'^budgetsituations/$', views.BudgetSituationsAPIListView.as_view()),
-  
-    url(r'^budgets/(?P<id>[0-9]+)$', views.BudgetsAPIView.as_view()),
-    url(r'^budgets/$', views.BudgetsAPIListView.as_view()),
-  
-    url(r'^clients/(?P<id>[0-9]+)$', views.ClientsAPIView.as_view()),
-    url(r'^clients/$', views.ClientsAPIListView.as_view()),
-  
-    url(r'^migrations/(?P<id>[0-9]+)$', views.MigrationsAPIView.as_view()),
-    url(r'^migrations/$', views.MigrationsAPIListView.as_view()),
-  
-    url(r'^registrationsituations/(?P<id>[0-9]+)$', views.RegistrationSituationsAPIView.as_view()),
-    url(r'^registrationsituations/$', views.RegistrationSituationsAPIListView.as_view()),
-  
-    url(r'^serviceproviders/(?P<id>[0-9]+)$', views.ServiceProvidersAPIView.as_view()),
-    url(r'^serviceproviders/$', views.ServiceProvidersAPIListView.as_view()),
-  
-    url(r'^servicetypes/(?P<id>[0-9]+)$', views.ServiceTypesAPIView.as_view()),
-    url(r'^servicetypes/$', views.ServiceTypesAPIListView.as_view()),
-  
-    url(r'^services/(?P<id>[0-9]+)$', views.ServicesAPIView.as_view()),
-    url(r'^services/$', views.ServicesAPIListView.as_view()),
-  
-    url(r'^usertypes/(?P<id>[0-9]+)$', views.UserTypesAPIView.as_view()),
-    url(r'^usertypes/$', views.UserTypesAPIListView.as_view()),
-  
-    url(r'^users/(?P<id>[0-9]+)$', views.UsersAPIView.as_view()),
-    url(r'^users/$', views.UsersAPIListView.as_view()),
-  
-  )
-else:
-  urlpatterns = [
+urlpatterns = [
   
     url(r'^addresses/(?P<id>[0-9]+)$', views.AddressesAPIView.as_view()),
     url(r'^addresses/$', views.AddressesAPIListView.as_view()),
