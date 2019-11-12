d043ff381d9578416c4e04de68f657587d49b850
Ivan <ivan.grego.lemos@gmail.com>
2019-11-12 10:14:34 -0300

    settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&_uzjk^k)vz3lgv^1+zrq%6gqyb!@e*-_p)5o$ejp0xem1_dqs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 20
 }
 
 # Database
